f2ff199c029d8f0af8c67bdf405ce59e815da496
Ubuntu <ubuntu@ip-172-31-40-132.us-east-2.compute.internal>
2019-09-24 23:32:08 +0000

    added scss processor

--- apetur/settings.py
+++ apetur/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sass_processor',
 ]
 
 MIDDLEWARE = [
@@ -106,4 +107,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+SASS_PROCESSOR_ROOT = STATIC_ROOT = os.path.join(BASE_DIR, "static/")
