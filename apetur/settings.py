75d1b6ca65b6da6272657553d729f7f2dd019446
Ubuntu <ubuntu@ip-172-31-40-132.us-east-2.compute.internal>
2019-09-25 03:43:21 +0000

    commented out sass-processor. since it was causing issue with apache web server

--- apetur/settings.py
+++ apetur/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sass_processor',
+#    'sass_processor',
 ]
 
 MIDDLEWARE = [
