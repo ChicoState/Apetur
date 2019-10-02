a338467fcededb9521e91a39031d6b2887b62a0b
Michael Wooll <michaelwooll@wlan-t-151-3-dhcp.CSUChico.EDU>
2019-10-02 14:28:27 -0700

    Added client and photographer models. Added these models to admin.py o allow for admin integartion.

--- apeturProject/admin.py
+++ apeturProject/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Client, Photographer
 # Register your models here.
+
+admin.site.register(Client)
+admin.site.register(Photographer)
--- apeturProject/models.py
+++ apeturProject/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    cityID = models.IntegerField()
+    stateID = models.IntegerField()
+    countryID = models.IntegerField()
+    def getFullName(self):
+        return self.user.first_name + " " + self.user.last_name
+    
+    def __str__(self):
+        return self.user.username
+
+
+class Photographer(models.Model):
+        client = models.OneToOneField(Client, on_delete = models.CASCADE)
+        bio = models.CharField(max_length = 500)
+        radius = models.PositiveIntegerField(default = 25)
+        tags = models.TextField()
+
+        def __str__(self):
+            return self.client.user.username
