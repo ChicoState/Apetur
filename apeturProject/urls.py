6bab9956a39fbbb821551d668085c1cb188334af
Youwei Yang <yyang2@mail.csuchico.edu>
2019-10-05 11:08:02 -0700

    created signup page layout. changes form name for both sign up and login form

--- apeturProject/urls.py
+++ apeturProject/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     # log in
     path('login', views.login),
 
+    # sign up
+    path('signup', views.signup),
+
     # browse
     path('browse', views.browse)
 ]
--- apeturProject/views.py
+++ apeturProject/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .models import Photographer
 
-
 # homepage
+
+
 def home(request):
     return render(request, 'home.html')
 
@@ -12,7 +13,21 @@ def login(request):
     return render(request, 'login.html')
 
 
+# sign up
+def signup(request):
+    return render(
+        request,
+        'signup.html',
+        {
+            'month_range': range(1, 13),
+            'date_range': range(1, 32),
+            'year_range': range(1950, 2020)
+        },
+    )
+
 # browse
+
+
 def browse(request):
     data = {
         'photographers': Photographer.objects.all()
