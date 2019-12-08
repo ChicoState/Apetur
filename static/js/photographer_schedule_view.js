9ab49bb83fd21f3cc214aeee07977db600dcb823
yyang31 <yyang2@mail.csuchico.edu>
2019-12-07 20:13:34 -0800

    relocating profile js and css

--- static/js/photographer_schedule_view.js
+++ static/js/photographer_schedule_view.js
@@ -3,22 +3,22 @@ var entries = Object.entries(event_json_data); // An array of entries. Each entr
 var event_data = [];
 var event_data_not_confirmed = [];
 
-for(var i = 0 ; i < entries.length; i++){
+for (var i = 0; i < entries.length; i++) {
     let date = entries[i][0];
     // Loop through events
     let events = entries[i][1];
-    for(var j = 0; j < events.length; j++){
+    for (var j = 0; j < events.length; j++) {
         let event = events[j];
-        let event_object =  {
+        let event_object = {
             title: event["client_name"],
-            start: date+"T"+event["start_time"],
-            end: date+"T"+event["end_time"]
-        }            
-        console.log(event);    
-        if(event["confirmed"]){
+            start: date + "T" + event["start_time"],
+            end: date + "T" + event["end_time"]
+        }
+        console.log(event);
+        if (event["confirmed"]) {
             event_data.push(event_object);
         } // end if confirmed
-        else{
+        else {
             event_object["id"] = event["id"];
             event_data_not_confirmed.push(event_object);
         }
@@ -26,7 +26,7 @@ for(var i = 0 ; i < entries.length; i++){
 }
 var calendarEl = document.getElementById('calendar');
 var calendar = new FullCalendar.Calendar(calendarEl, {
-    plugins: [ 'dayGrid', 'timeGrid'],
+    plugins: ['dayGrid', 'timeGrid'],
     header: {
         center: 'dayGridMonth,timeGridWeek,timeGridDay' // buttons for switching between views
     },
